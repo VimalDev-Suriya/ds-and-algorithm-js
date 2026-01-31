d121fd3c84dda4609b1345736b4dd14846b92c4f
Suriya <64723101+VimalDev-Suriya@users.noreply.github.com>
2026-01-31 15:22:50 +0530

    Create infix-prefix-postfix.js

---
+++ src/stack/problems/infix-prefix-postfix.js
@@ -0,0 +1,8 @@
+/*
+* handling the arithmetic operations using Stack:
+* Operators = +, -, / ^, *
+* Operands = 0-9, a-z
+* Infix = (a+b) * (c-d) => all operators are inbteween the operands
+* prefix = x + ab - cd => all operators are before & ab is multiply of a
+* postfix = ab - cd + y
+*/ 
