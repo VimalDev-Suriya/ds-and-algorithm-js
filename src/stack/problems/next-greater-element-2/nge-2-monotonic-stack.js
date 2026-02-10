a9eff100a7d734155decce8dc4737efa16e16920
Suriya <64723101+VimalDev-Suriya@users.noreply.github.com>
2026-02-10 22:54:15 +0530

    Add Next Greater Element - 2 implementation
    Implement Next Greater Element - 2 algorithm using a monotonic stack.

---
+++ src/stack/problems/next-greater-element-2/nge-2-monotonic-stack.js
@@ -0,0 +1,26 @@
+// * Next Greater Element - 2 (using monotonic stack)
+
+const nge2 = arr => {
+    const result = [];
+    const stack = [];
+    const n = arr.length;
+
+  // * Same hypothetical array considerations
+    for(let i=(2*n) -1; i>=0; i--){
+        while(stack.length && stack[stack.length - 1] <= arr[i%n]){
+            stack.pop();
+        }
+
+      // * Here I am simply going to push the nge elements to result only if i is getting into original array 
+        if(i < n) {
+            result[i] = stack.length === 0 ? -1: stack[stack.length - 1]; // -1 or stack's top element
+        }
+
+        stack.push(arr[i%n]); // % since always dealing with hypothetical array
+    }
+
+    return result;
+}
+
+nge2([2, 10, 12, 1, 11]); // [10, 12, -1, 11, 12]
+// [2, 10, 12, 1, 11] 2(5), 10(6), 12(7), 1(8), 11(9). 
